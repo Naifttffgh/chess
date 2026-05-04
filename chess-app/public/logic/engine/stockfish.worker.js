40a302919716dd964b80746bdd9f27d5cb2003eb
Naifttffgh <nayf87541@gmail.com>
2026-05-04 21:07:33 +0300

    Delete chess-app directory

--- chess-app/public/logic/engine/stockfish.worker.js
+++
@@ -1,8 +0,0 @@
-// استدعاء ملف المحرك الرئيسي
-importScripts('stockfish-nnue-16.js');
-
-// بدء تشغيل المحرك
-self.onmessage = (e) => {
-  // تمرير الرسائل من React إلى المحرك المكتوب بلغة C++/Wasm
-  self.postMessage(e.data);
-};
