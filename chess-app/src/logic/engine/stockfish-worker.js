f4816a963824b77634dd801df28e20280d7cd3fc
Naifttffgh <nayf87541@gmail.com>
2026-05-04 20:36:04 +0300

    Delete chess-app directory

--- chess-app/src/logic/engine/stockfish-worker.js
+++
@@ -1,9 +0,0 @@
-// استيراد مكتبة Stockfish 16 (يجب تحميل الملف في public/logic/engine/)
-importScripts('/logic/engine/stockfish-nnue-16.js');
-
-// استلام الأوامر من الموقع (من الـ Hook)
-onmessage = function(e) {
-  const command = e.data;
-  // إرسال الأمر للمحرك
-  postMessage("الرد من المحرك على: " + command);
-};
