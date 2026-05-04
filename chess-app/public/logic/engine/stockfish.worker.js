a7c93b154900446bcaf9cb8984c5072bc3f90b88
Naifttffgh <nayf87541@gmail.com>
2026-05-04 21:00:08 +0300

    Initialize Stockfish engine worker with import script

--- chess-app/public/logic/engine/stockfish.worker.js
+++ chess-app/public/logic/engine/stockfish.worker.js
@@ -1,1 +1,8 @@
+// استدعاء ملف المحرك الرئيسي
+importScripts('stockfish-nnue-16.js');
 
+// بدء تشغيل المحرك
+self.onmessage = (e) => {
+  // تمرير الرسائل من React إلى المحرك المكتوب بلغة C++/Wasm
+  self.postMessage(e.data);
+};
